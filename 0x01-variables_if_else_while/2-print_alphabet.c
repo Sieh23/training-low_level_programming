042c313c16299bd64bd1c1a04f5b9786901a17ea
Sieh23 <ndorboryallah33@gmail.com>
2025-03-05 23:42:14 +0000

    Wrote a code the listed all the small alphabet charac.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+           char ch = 97;
+            while (ch <= 122){
+	    putchar(ch);
+             ch++;
+	    }
+	    putchar('\n');
+
+	    return 0;
+
+}
