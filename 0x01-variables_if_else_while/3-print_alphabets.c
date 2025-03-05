752f964511a65f51cb3b1c1b69cf63b600eaa780
Sieh23 <ndorboryallah33@gmail.com>
2025-03-05 21:55:48 +0000

    I change the files name to the rightful ones for task 3/4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+
+     int ch = 97;
+       char a = 65;
+    while (ch <= 122){
+            putchar(ch);
+            ch++;
+    }
+   while (a <= 90){
+     putchar(a);
+    a++;
+  }
+   putchar('\n');
+ return 0;
+
+}
