53c25dfb09cdf39fa5881e958021a8eabc9c264a
Sieh23 <ndorboryallah33@gmail.com>
2025-03-06 15:04:00 +0000

    Day_3 task_9 omotting the five vowels

---
+++ 0x01-variables_if_else_while/9-print_alphabt_skip_vowels_extended.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+
+         char ch = 97;
+         
+         while(ch <= 122){
+	 if(ch == 97 || ch == 101 || ch == 105 || ch == 111 || ch == 117 ){
+	 ch++;
+	 continue;
+	 }
+	 putchar(ch);
+	 ch++;
+	 }
+         
+	 putchar('\n');
+
+         return 0;
+
+}
