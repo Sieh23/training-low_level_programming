2aa750133004bb5faebe83d574a76826c8f914e9
Sieh23 <ndorboryallah33@gmail.com>
2025-03-07 14:28:59 +0000

    Wrote a c code that skip vowels with the consonant

--- 0x01-variables_if_else_while/9-print_alphabt_skip_vowels_extended.c
+++ 0x01-variables_if_else_while/9-print_alphabt_skip_vowels_extended.c
@@ -2,16 +2,24 @@
 
 int main(void){
 
-         char ch = 97;
-         
-         while(ch <= 122){
-	 if(ch == 97 || ch == 101 || ch == 105 || ch == 111 || ch == 117 ){
-	 ch++;
-	 continue;
+		char ch = 97;
+          	char letters[] = "Vowels Skipped: 5";
+          int y = 0;
+
+	while(ch <= 122){
+			 if(ch == 97 || ch == 101 || ch == 105 || ch == 111 || ch == 117 ){
+					 ch++;
+			 continue;
+			 }
+			 putchar(ch);
+			 
+			 ch++;
 	 }
-	 putchar(ch);
-	 ch++;
-	 }
+	 putchar('\n');; 
+
+	for (; letters[y] != '\0'; y++){
+			putchar(letters[y]);
+	}
          
 	 putchar('\n');
 
