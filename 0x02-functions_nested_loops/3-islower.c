41135c6b6c73bfdac1810ba1a603dde90c75fc0a
Sieh23 <ndorboryallah33@gmail.com>
2025-04-03 03:51:50 +0000

    Returns 1 if it encounters a lowercase letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+
+int _islower(int c)
+{
+
+	if (c == 'o')
+	{
+	return 1;
+	}
+
+	else if (c == 108)
+	{
+	return 1;
+	}
+
+	else
+	{
+	return 0;
+	}
+	
+	return 0;
+}
