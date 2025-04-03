78c857f78e0bfb7a6bfcbd0a2b566d7dedb7e0b0
Sieh23 <ndorboryallah33@gmail.com>
2025-04-03 03:56:11 +0000

    Contain the prototypes of all the codes

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+
+int _putchar(char c);
+int add(int, int);
+void times_table(void);
+void print_alphabet_x10(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
+int _islower(int c);
+void print_to_98(int n);
+void print_alphabet(void);
+
+
+
+#endif
