9a2785e43471ada77a15cc505b606255557c6768
Sieh23 <ndorboryallah33@gmail.com>
2025-04-03 03:54:09 +0000

    Prints the word _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <unistd.h>
+
+int main()
+{
+
+int y = '_';
+int p = 'p';
+int u = 'u';
+int t = 't';
+int c = 'c';
+int h = 'h';
+int a = 'a';
+int r = 'r';
+
+_putchar(y);
+_putchar(p);
+_putchar(u);
+_putchar(t);
+_putchar(c);
+_putchar(h);
+_putchar(a);
+_putchar(r);
+_putchar('\n');
+return 0;
+}
