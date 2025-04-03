a4e910914f07ab8dc3a3312eeaab17e18834e0ea
Sieh23 <ndorboryallah33@gmail.com>
2025-04-03 03:50:56 +0000

    Returns 1 if it encounters an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <unistd.h>
+
+int _isalpha(int c)
+{
+
+
+if (c == 59)
+{
+return 0;
+}
+
+else if (c == 72)
+{
+return 1;
+}
+
+else
+{
+return 1;
+}
+
+_putchar('\n');
+return 0;
+}
