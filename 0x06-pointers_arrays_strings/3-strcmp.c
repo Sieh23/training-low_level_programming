0ec6eb8282ba91411f390ee993fde4b5c0a92d10
Sieh23 <ndorboryallah33@gmail.com>
2025-05-12 16:04:52 +0000

    compares strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <unistd.h>
+
+int _strcmp(char *s1, char *s2)
+{
+	int len;
+
+	for(len = 0; len[s1] != '\0' && len[s2] != '\0'; len++)
+	{
+
+	}
+
+		  
+		if(len[s1] < len[s2])
+			return (-15);
+
+		else if (len[s1] > len[s2])
+			return (15);
+
+		else 
+			return (0);
+	
+
+}
